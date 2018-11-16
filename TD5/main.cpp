7c4f8953ab9cbcde167613593912218314613ca8
sofos-alvagonza <sofia.alva.gonza@gmail.com>
2018-11-16 17:05:31 -0500

    update

--- TD5/main.cpp
+++ TD5/main.cpp
@@ -172,12 +172,20 @@ int main ( )
 	Cible c11 = {11, {38.140728, -76.426494}, "Triangle gris, O orange", "cible_11.jpg"};
 	
 	// TODO: Lire le journal de détection "Cibles.data".
-	
+	bool ok;
+	JournalDetection journal = lireJournalDetection(nomFichierCibles, ok);
+
 	// TODO: Faire la vérification d'erreur et terminer le programme avec un
 	//       message s'il y a erreur.
+	if (ok == false) {
+		cout << endl << "Erreur de lecture!" << endl;
+	}
 	
 	// TODO: Afficher le journal.  (Devrait afficher un journal avec 10 cibles ayant des données valides.)
 	
+	
+
+
 	// TODO: Retirer la cible 5 de la liste du journal.
 	// TODO: Ajouter la cible 11 (variable 'c11' ci-dessus) à la liste du journal.
 	// TODO: Afficher les cibles pour vérifier que les opérations ci-dessus ont bien fonctionnées.
